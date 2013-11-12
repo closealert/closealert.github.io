79b64b6267160e287644c7b79ea2631ddb3c533f
Joost de Vries <joost@closealert.com>
2013-11-12 14:27:26 +0100

    Added generator

---
+++ javascripts/docs.js
@@ -0,0 +1,32 @@
+(function($) {
+  var id = 'YOUR_ID';
+  $(document).ready(function() {
+
+    // Read id from hash
+    if(document.location.hash && document.location.hash.indexOf('id=')!==-1) {
+      id = document.location.hash.split('id=')[1];
+      $('#setting-clslrtid').val(id);
+    }
+
+    function updateSetting(settingInputElementId, value) {
+      if(typeof value=='undefined') {
+        value = $('#'+settingInputElementId).val();
+      }
+
+      $('#'+settingInputElementId+'-output').show();
+      if(value!=='default')
+        $('#'+settingInputElementId+'-output').html("\n__clslrt.setting('"+settingInputElementId.replace('setting-','')+"', '"+value+"');");
+      else
+        $('#'+settingInputElementId+'-output').hide();
+    }
+
+    $('form#customize :input').on('change', function() {
+      updateSetting($(this).attr('id'), $(this).val());
+    });
+
+    updateSetting('setting-clslrtid');
+    updateSetting('setting-icons');
+    updateSetting('setting-lang');
+    updateSetting('setting-position');
+  });
+})(jQuery);
