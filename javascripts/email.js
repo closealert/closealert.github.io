c660b92be7bd21400d3d9f567fd1c09b4e79fc1d
Joost de Vries <joost@closealert.com>
2014-04-28 15:15:45 +0200

    Tet

---
+++ javascripts/email.js
@@ -0,0 +1,29 @@
+(function($) {
+  var id = 'YOUR_ID';
+  $(document).ready(function() {
+
+    // Read id from hash
+    if(document.location.hash && document.location.hash.indexOf('id=')!==-1) {
+      id = document.location.hash.split('id=')[1];
+      $('#setting-id').val(id);
+    }
+
+    function updateSetting(settingInputElementId, value) {
+      if(typeof value=='undefined') {
+        value = $('#'+settingInputElementId).val();
+      }
+      $('#'+settingInputElementId+'-output').html(value);
+    }
+
+    $('form#customize :input').on('change', function() {
+      updateSetting($(this).attr('id'), $(this).val());
+    });
+
+    updateSetting('setting-id');
+    // updateSetting('setting-icons');
+    // updateSetting('setting-language');
+    // updateSetting('setting-position');
+    // updateSetting('setting-noemail');
+    // updateSetting('setting-mobile');
+  });
+})(jQuery);
